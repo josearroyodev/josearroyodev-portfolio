a88eaffc97f3cede9c990556ce68795e59caef4f
JoseArroyoDev <jose@josearroyo.dev>
2026-01-27 10:40:44 +0100

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,56 @@
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["eslint:recommended", "plugin:astro/recommended", "prettier"],
-  overrides: [
-    {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
+import js from "@eslint/js";
+import eslintPluginAstro from "eslint-plugin-astro";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
     },
-  ],
-  rules: {
-    "no-unused-vars": "warn",
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
   },
-};
+
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
+  },
+
+  {
+    ignores: [
+      "dist/",
+      "node_modules/",
+      ".astro/",
+      "build/",
+      ".env",
+      ".env.*",
+      "!.env.example",
+    ],
+  },
+];
